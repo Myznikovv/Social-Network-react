d70bb4e73f0de01533e2168ade41163f4e62e4ef
Myznikov Vitaly <myzvitalik@yandex.ru>
2022-07-10 19:23:46 +0300

    Первичное дробление на компоненты

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return (<header className='header'>
+        <img src='https://infoline.spb.ru/upload/medialibrary/2cb/2cbc84597f87e4968cc4fa5e329fd433.png' height='40px'/>
+    </header>);
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () => {
+    return (<div className='content  '>
+        <div>
+            <img
+                src='https://sun9-30.userapi.com/impf/TgJ-7xWS6fUdvCPWabpUvZXwAqRu0_9YV7pSUA/qWHBmk3u_KU.jpg?size=795x265&quality=95&crop=0,0,1590,530&sign=787d108af566aaad5ff78a4162c103d8&type=cover_group'/>
+        </div>
+        <div>
+            ava+ description
+        </div>
+        <div>
+            My posts
+            <div>
+                New posts
+            </div>
+            <div>Post 1</div>
+            <div>Post2</div>
+        </div>
+    </div>);
+}
+
+export default Profile;
